6291002911ce3dffe66bdc70feb057c2c5c1f635
jgnatch <nachojgutierrez@gmail.com>
2014-08-27 12:53:50 -0300

    new aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,7 +9,10 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gps="git push"
+alias ga="git add"
+alias gp="git push"
+alias gcm="git commit -m"
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
